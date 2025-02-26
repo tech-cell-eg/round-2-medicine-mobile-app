04bb1930ef943faea76dd621df96fcbb4b4c4385
Amr Ali <140944803+Amr-3li@users.noreply.github.com>
2025-02-26 09:57:12 +0200

    make file end points

--- lib/core/constants/end_points.dart
+++ lib/core/constants/end_points.dart
@@ -1,3 +1,10 @@
 class EndPoints {
-  static const String baseUrl = 'https://api.openweathermap.org/data/2.5/';
+  static const String baseUrl =
+      'https://round-2-medicine.digital-vision-solutions.com/api/';
+  static const String productDetails = 'products/';
+  static const String notifications = 'notifications';
+  static const String categories = 'categories/';
+  static const String products = 'products';
+  static const String addToCart = 'cart/add';
+  static const String cart = 'cart';
 }
